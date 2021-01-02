7990aa8c0c2d751bca1bcde4a1d2c6fd00ec5e9f
taemin <taemin9705@gmail.com>
2021-01-02 23:40:33 +0900

    틀 제작 완료 및 테스트 학습 완료

--- multi_class_img.py
+++ multi_class_img.py
@@ -72,3 +72,95 @@ session = tf.compat.v1.Session(config=config)
 X_train, X_test, y_train, y_test = np.load('C:/Users/taemin/PycharmProjects/What-sYourMerchandise/multi_image_data.npy')
 print(X_train.shape)
 print(X_train.shape[0])
+
+categories = ["Cocacola", "sevenstar", "sprite"]
+nb_classes = len(categories)
+
+#일반화
+X_train = X_train.astype(float) / 255
+X_test = X_test.astype(float) / 255
+
+model = Sequential()
+model.add(Conv2D(32, (3,3), padding="same", input_shape=X_train.shape[1:], activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3,3), padding="same", activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model_dir = './model'
+
+if not os.path.exists(model_dir):
+    os.mkdir(model_dir)
+
+model_path = model_dir + '/multi_img_classification.model'
+checkpoint = ModelCheckpoint(filepath=model_path , monitor='val_loss', verbose=1, save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_loss', patience=6)
+
+model.summary()
+
+history = model.fit(X_train, y_train, batch_size=32, epochs=50, validation_data=(X_test, y_test), callbacks=[checkpoint, early_stopping])
+
+print("정확도 : %.4f" % (model.evaluate(X_test, y_test)[1]))
+
+y_vloss = history.history['val_loss']
+y_loss = history.history['loss']
+
+x_len = np.arange(len(y_loss))
+
+plt.plot(x_len, y_vloss, marker='.', c='red', label='val_set_loss')
+plt.plot(x_len, y_loss, marker='.', c='blue', label='train_set_oss')
+plt.legend()
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.grid()
+plt.show()
+
+from PIL import Image
+import os, glob, numpy as np
+from keras.models import load_model
+
+caltech_dir = "C:/Users/taemin/PycharmProjects/What-sYourMerchandise/DataSet/Test"
+image_w = 64
+image_h = 64
+
+pixels = image_h * image_w * 3
+
+X = []
+filenames = []
+files = glob.glob(caltech_dir+"/*.*")
+for i, f in enumerate(files):
+    img = Image.open(f)
+    img = img.convert("RGB")
+    img = img.resize((image_w, image_h))
+    data = np.asarray(img)
+    filenames.append(f)
+    X.append(data)
+
+X = np.array(X)
+model = load_model('./model/multi_img_classification.model')
+
+prediction = model.predict(X)
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+cnt = 0
+
+
+for i in prediction:
+    pre_ans = i.argmax()  # 예측 레이블
+    print(i)
+    print(pre_ans)
+    pre_ans_str = ''
+    if pre_ans == 0: pre_ans_str = "코카콜라"
+    elif pre_ans == 1: pre_ans_str = "칠성사이다"
+    elif pre_ans == 2: pre_ans_str = "스프라이트"
+    else: pre_ans_str = "게"
+    if i[0] >= 0.8 : print("해당 "+filenames[cnt].split("\\")[1]+"이미지는 "+pre_ans_str+"로 추정됩니다.")
+    if i[1] >= 0.8: print("해당 "+filenames[cnt].split("\\")[1]+"이미지는 "+pre_ans_str+"으로 추정됩니다.")
+    if i[2] >= 0.8: print("해당 "+filenames[cnt].split("\\")[1]+"이미지는 "+pre_ans_str+"로 추정됩니다.")
+    cnt += 1
